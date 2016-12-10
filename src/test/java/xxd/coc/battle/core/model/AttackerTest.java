642585d2af25a785c710de3936fc5031202388cc
xuanbei lu <1762175@qq.com>
2016-12-10 20:41:00 +0800

    test

--- src/test/java/xxd/coc/battle/core/model/AttackerTest.java
+++ src/test/java/xxd/coc/battle/core/model/AttackerTest.java
@@ -1,11 +1,23 @@
 package xxd.coc.battle.core.model;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import junit.framework.TestCase;
 
 public class AttackerTest extends TestCase {
 	
 	
 	public void testLeftAttackChance() {
-		assertEquals(1,1);
+		Map<String, Integer> confidence = new HashMap<String, Integer>();
+		confidence.put("2", 2);
+		confidence.put("1", 3);
+		Set<String> attacked = new HashSet<String>();
+		attacked.add("1");
+		Attacker a = new Attacker("1", confidence, attacked);
+		assertEquals(a.getLeftAttackChance(), 1);
 	}
+
 }
