ec6f2a68931ceadd3ff8064cfcd3de62e6abfcae
xuanbei lu <1762175@qq.com>
2016-12-10 20:32:27 +0800

    test

---
+++ src/test/java/xxd/coc/battle/core/model/AttackerTest.java
@@ -0,0 +1,11 @@
+package xxd.coc.battle.core.model;
+
+import junit.framework.TestCase;
+
+public class AttackerTest extends TestCase {
+	
+	
+	public void testLeftAttackChance() {
+		assertEquals(1,2);
+	}
+}
